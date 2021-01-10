5f3e9a61483a1e77649f597a20c251fe63df06c2
EZFN <admin@ezfn.dev>
2021-01-10 12:19:54 +0100

    Fixed something

--- auth.py
+++ auth.py
@@ -9,24 +9,23 @@ import configparser
 
 def get_auth_token():
     GameUserSettingsPath = 'C:\\Users\\' + os.getlogin() + '\\AppData\\Local\\EpicGamesLauncher\\Saved\\Config\\Windows\\GameUserSettings.ini'
-    config_text = open(GameUserSettingsPath, encoding="utf-16le").read().replace("\ufeff","")
-    GameUserSettings = configparser.ConfigParser(strict=False)
-    GameUserSettings.read_string(config_text)
-
-    old_refresh_token = ''
-    if 'RememberMe' in GameUserSettings.sections():
-        if GameUserSettings['RememberMe']['Enable'] == 'True':
-            data = GameUserSettings['RememberMe']['Data']
-            old_data = data
-            decoded_data = base64.b64decode(data).decode('ascii')
-            decoded_data = json.loads(decoded_data)
-            old_refresh_token = decoded_data[0]['Token']
+    config_text = open(GameUserSettingsPath).read()
+    
+    if 'Data=' in config_text:
+        data = config_text.split('Data=')[1].split('\n')[0]
+        old_data = data
+        decoded_data = base64.b64decode(data).decode('ascii')
+        decoded_data = json.loads(decoded_data)
+        old_refresh_token = decoded_data[0]['Token']
 
     if old_refresh_token:
         data = requests.post('https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',headers={'Authorization': 'basic MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y='},data={'token_type': 'eg1','grant_type': 'refresh_token','refresh_token': old_refresh_token}).json()
 
         if not 'refresh_token' in data:
-            raise Exception('Failed to authorize using the refresh token!')
+            if data['errorCode'] == 'errors.com.epicgames.account.auth_token.invalid_refresh_token':
+                raise Exception('Failed to authorize using the refresh token!')
+            else:
+                raise Exception(f"{data['errorCode']} bruh, what?")                
 
         new_refresh_token = data['refresh_token']
         new_data = decoded_data
@@ -35,9 +34,8 @@ def get_auth_token():
 
         new_data = base64.b64encode(json.dumps(new_data).encode("utf-8")).decode("utf-8")
 
-        config_text = open(GameUserSettingsPath, encoding="utf-16le").read()
         config_text = config_text.replace(old_data, new_data)
-        open(GameUserSettingsPath, 'w+',  encoding="utf-16le").write(config_text)
+        open(GameUserSettingsPath, 'w+').write(config_text)
 
         exchange_code = requests.get(f'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange',headers={'Authorization': f'bearer {data["access_token"]}','Content-Type': 'application/json'}).json()
         fn_access_token = requests.post(f'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',headers = {'Authorization': 'basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ='},data = {'grant_type': 'exchange_code','token_type': 'bearer','exchange_code': exchange_code['code']}).json()
